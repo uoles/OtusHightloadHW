04824d81dc47f2ed7c00d3d998403a79086668ce
Kulikov Maksim <max.uoles@rambler.ru>
2022-03-31 07:57:18 +0300

    HW-1 Рефакторинг.

--- src/main/java/ru/uoles/proj/database/mapper/PersonMapper.java
+++ src/main/java/ru/uoles/proj/database/mapper/PersonMapper.java
@@ -16,20 +16,33 @@ import java.util.Date;
  */
 public class PersonMapper implements RowMapper<Person> {
 
+    private static final String ID = "id";
+    private static final String REGISTRATION_DATE = "registration_date";
+    private static final String DISSOLUTION_DATE = "dissolution_date";
+    private static final String PERMISSIONS_ID = "permissions_id";
+    private static final String FIRST_NAME = "first_name";
+    private static final String SECOND_NAME = "second_name";
+    private static final String LOGIN = "login";
+    private static final String SECRET = "secret";
+    private static final String INFO = "info";
+    private static final String AGE = "age";
+    private static final String GENDER = "gender";
+    private static final String TOWN = "town";
+
     @Override
     public Person mapRow(ResultSet rs, int rowNum) throws SQLException {
-        final Long id = rs.getLong("id");
-        final Date registrationDate = rs.getDate("registration_date");
-        final Date dissolutionDate = rs.getDate("dissolution_date");
-        final int permissionsId = rs.getInt("permissions_id");
-        final String firstName = rs.getString("first_name");
-        final String secondName = rs.getString("second_name");
-        final String login = rs.getString("login");
-        final String secret = rs.getString("secret");
-        final String info = rs.getString("info");
-        final int age = rs.getInt("age");
-        final String gender = rs.getString("gender");
-        final String town = rs.getString("town");
+        final Long id = rs.getLong(ID);
+        final Date registrationDate = rs.getDate(REGISTRATION_DATE);
+        final Date dissolutionDate = rs.getDate(DISSOLUTION_DATE);
+        final int permissionsId = rs.getInt(PERMISSIONS_ID);
+        final String firstName = rs.getString(FIRST_NAME);
+        final String secondName = rs.getString(SECOND_NAME);
+        final String login = rs.getString(LOGIN);
+        final String secret = rs.getString(SECRET);
+        final String info = rs.getString(INFO);
+        final int age = rs.getInt(AGE);
+        final String gender = rs.getString(GENDER);
+        final String town = rs.getString(TOWN);
 
         return new Person(id, registrationDate, dissolutionDate, permissionsId, firstName, secondName,
                 login, secret, info, age, gender, town
