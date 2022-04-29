71f06b131393b417dbfbf3341619ea3ca5cc2615
Kulikov Maksim <max.uoles@rambler.ru>
2022-04-29 19:11:19 +0300

    HW-2 Рефакторинг.

--- build.sh
+++ build.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-
 git pull
 mvn clean install
-
-docker build -t uoles/mooncake-social .
-docker run --name social-project -p 8081:8081 --network=host -d uoles/mooncake-social
-
-exit $?
