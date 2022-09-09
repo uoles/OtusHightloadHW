96077d71e07b84a3208fb4cbb88898bbff16e25b
Kulikov Maksim <max.uoles@rambler.ru>
2022-09-09 08:15:31 +0300

    HW-4 Добавление websocket.

--- src/main/resources/static/js/websocket.js
+++ src/main/resources/static/js/websocket.js
@@ -1,10 +1,17 @@
+'use strict';
+
 var stompClient = null;
+
+// var messageArea = document.querySelector("#messageArea");
+var colors = [
+    '#2196F3', '#32c787', '#00BCD4', '#ff5652',
+    '#ffc107', '#ff85af', '#FF9800', '#39bbb0'
+];
 
 function setConnected(connected) {
     document.getElementById('connect').disabled = connected;
     document.getElementById('disconnect').disabled = !connected;
     document.getElementById('conversationDiv').style.visibility = connected ? 'visible' : 'hidden';
-    document.getElementById('response').innerHTML = '';
 }
 
 function connect() {
@@ -13,9 +20,7 @@ function connect() {
     stompClient.connect({}, function(frame) {
         setConnected(true);
         console.log('Connected: ' + frame);
-        stompClient.subscribe('/queue/chat', function(greeting){
-            showGreeting(JSON.parse(greeting.body).text, JSON.parse(greeting.body).name);
-        });
+        stompClient.subscribe('/queue/chat', onMessageReceived);
     });
 }
 
@@ -36,10 +41,49 @@ function sendName() {
     );
 }
 
-function showGreeting(message, name) {
-    var response = document.getElementById('response');
-    var p = document.createElement('p');
-    p.style.wordWrap = 'break-word';
-    p.appendChild(document.createTextNode(message + " / " + name));
-    response.appendChild(p);
+// function showGreeting(message, name) {
+//     var response = document.getElementById('response');
+//     var p = document.createElement('p');
+//     p.style.wordWrap = 'break-word';
+//     p.appendChild(document.createTextNode(message + " / " + name));
+//     response.appendChild(p);
+// }
+
+function onMessageReceived(input) {
+    var message = JSON.parse(input.body);
+
+    var messageElement = document.createElement('li');
+    messageElement.classList.add('chat-message');
+
+    var avatarElement = document.createElement('i');
+    var avatarText = document.createTextNode(message.name[0]);
+    avatarElement.appendChild(avatarText);
+    avatarElement.style['background-color'] = getAvatarColor(message.name);
+
+    messageElement.appendChild(avatarElement);
+
+    var usernameElement = document.createElement('span');
+    var usernameText = document.createTextNode(message.name);
+    usernameElement.appendChild(usernameText);
+    messageElement.appendChild(usernameElement);
+
+    var textElement = document.createElement('p');
+    var messageText = document.createTextNode(message.text);
+    textElement.appendChild(messageText);
+
+    messageElement.appendChild(textElement);
+
+    var messageArea = document.getElementById('messageArea');
+    messageArea.appendChild(messageElement);
+    messageArea.scrollTop = messageArea.scrollHeight;
+}
+
+function getAvatarColor(messageSender) {
+    var hash = 0;
+    for (var i = 0; i < messageSender.length; i++) {
+        hash = 31 * hash + messageSender.charCodeAt(i);
+    }
+
+    var index = Math.abs(hash % colors.length);
+    return colors[index];
 }
